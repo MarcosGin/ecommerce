c41317b942bba55cc18e1f5df3ca62b65cdde8d4
= <marcosgin291@gmail.com>
2017-06-18 15:24:23 -0300

    Updates for first view

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -4,4 +4,13 @@
  * User: marcos
  * Date: 18/06/17
  * Time: 13:52
- */ 
+ */
+
+namespace App\Controllers;
+
+class IndexController extends BaseController{
+
+    public function getIndex(){
+        return $this->render('index.twig', []);
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+##Front Controller
+
+//Show errors
+ini_set('display_errors', true);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Phroute\Phroute\RouteCollector;
+
+//Url directory
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL', $baseUrl);
+
+//Init Environment Variables
+$dotenv = new \Dotenv\Dotenv(__DIR__ . '/..');
+$dotenv->load();
+
+//Init Database
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+//Routes
+$route = $_GET['route'] ?? '/';
+$router = new RouteCollector();
+
+$router->controller('/', App\Controllers\IndexController::class);
+
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+echo $response;
