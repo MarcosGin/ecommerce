54b4619ba1d725fea5e8a9565e5ce2eeb8ad89c8
Marcos Gin <marcosgin291@gmail.com>
2017-07-10 12:55:47 -0300

    We send the cookie with the token in the headers. We show the messages we receive from the controller.

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -14,9 +14,23 @@ $(function () {
             url: 'http://localhost/ecommerce/public/products/comment',
             type: 'POST',
             data: form.serialize(),
-            datatype: 'json'
+            datatype: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", Cookies.get('__token'));
+            }
        }).done(function (data) {
-           console.log(data);
+           if(data.result === true){
+               Cookies.set('__token', data.jwt);
+               message.html('<div id="Success">' +
+                   '<p> <i class="fa fa-exclamation-triangle"></i>  '+ data.message + ' </p>' +
+                   '</div>');
+               message.css({'display': 'block'});
+           }else{
+               message.html('<div id="Failed">' +
+                   '<p> <i class="fa fa-exclamation-triangle"></i> '+ data.message + '</p>' +
+                   '</div>');
+               message.css({'display': 'block'});
+           }
        }).fail(function (jqXHR, textStatus, errorThrown) {
            console.log(jqXHR.responseText + "\n" + textStatus + "\n" + errorThrown);
        });
