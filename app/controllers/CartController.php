e866b825f8e320d6088b57f48bd3c5156ab409e6
Marcos Gin <marcosgin291@gmail.com>
2017-07-12 14:56:16 -0300

    We return an array with the new data of the cart.

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -2,15 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\Product;
+
 class CartController extends BaseController{
+    private $product;
+    public function __construct(){
+        parent::__construct();
+        $this->product = new Product();
+    }
 
     public function anyIndex(){
         return $this->render('cart.twig', []);
     }
 
     public function postMycart(){
+        $product = $this->product->getProduct($_POST['product_id']);
 
-        echo $this->json_response($_POST, 200);
+        if($product['result']){
+            try {
+                $cart = $_POST['cart'] ?? [];
+                $exits = false;
+                foreach ($cart as $key => $value) {
+                    if ($key === md5($_POST['product_id'])) {
+                        $cart[$key]['quantity'] += 1;
+                        $exits = true;
+                    }
+                }
+                if (!$exits) {
+                    $cart[md5($product['response'][0]->id)] = array('id' => $product['response'][0]->id,
+                        'name' => $product['response'][0]->nombre,
+                        'price' => $product['response'][0]->precio,
+                        'quantity' => 1);
+                }
+                echo $this->json_response($cart, 200, '', true);
+            }catch (\Exception $e){
+                echo $this->json_response('An unexpected error occurred, please try again', 200);
+            }
+        }else{
+            echo $this->json_response($product['response'], 200);
+        }
+
     }
 
 }
