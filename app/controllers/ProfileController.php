617ed3fd0d07ec6d866c78353289b0da56d97198
Marcos Gin <marcosgin291@gmail.com>
2017-07-20 02:30:29 -0300

    It is pending to finish this.

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -7,5 +7,9 @@ class ProfileController extends BaseController{
     public function getIndex(){
         return $this->render('profile/index.twig', []);
     }
+    public function putSettings(){
+        parse_str(file_get_contents("php://input"),$post_vars);//data amazing
 
+        echo $this->json_response($post_vars, 200);
+    }
 }
