ca7ad6353f341635d9ad8ac4ea79fd9ae00892cb
Marcos Gin <marcosgin291@gmail.com>
2017-07-22 22:18:31 -0300

    We send the data by the headers

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Bin\Token;
+
 class ProfileController extends BaseController{
 
     public function getIndex(){
@@ -10,6 +12,12 @@ class ProfileController extends BaseController{
     public function putSettings(){
         parse_str(file_get_contents("php://input"),$post_vars);//data amazing
 
+        $jwt = [];
+        foreach (getallheaders() as $key => $value){
+            if($key == 'Authorization'){
+                $jwt = Token::checkToken($value);
+            }
+        }
         echo $this->json_response($post_vars, 200);
     }
 }
