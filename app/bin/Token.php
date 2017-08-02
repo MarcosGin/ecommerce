8da2adbf826120f9bec70756fc720a32d5672ab2
Marcos Gin <marcosgin291@gmail.com>
2017-08-02 15:43:54 -0300

    We no longer need this function, we checked in db, if jwt exists and if it is enabled.

--- app/bin/Token.php
+++ app/bin/Token.php
@@ -7,6 +7,7 @@
  */
 namespace App\Bin;
 
+use App\Bin\Database\DB;
 
 class Token
 {
@@ -20,60 +21,47 @@ class Token
        return  \Firebase\JWT\JWT::encode($token, SECRET_KEY, ALGORITHM);
     }
 
-    public static function updateDataToken($token, $newData){
-
-        $chkToken = self::checkToken($token);
-
-            if($chkToken['result']){
-                $jwt = \Firebase\JWT\JWT::decode($chkToken['token'], SECRET_KEY, array(ALGORITHM));
-                $time = $jwt->exp - time();
-
-                foreach ($newData as $key => $value){
-                    foreach ($chkToken['jwt'] as $key2 => $value2){
-                        if($key === $key2){
-                            $chkToken['jwt']->$key2 = $value;
-                        }
-                    }
-                }
-                $data = json_decode(json_encode($chkToken['jwt']), true);
-                $newJwt = self::newToken($data, $time);
-                $token = $newJwt;
-            }
-
-        return $token;
-    }
-
-
     public static function checkToken($token){
-
         $message = array('result' => false);
-        if(!$token){
-            $message['response'] = 'Token empty!';
-            return $message;
-        }
-
-        try{
-            $jwt = \Firebase\JWT\JWT::decode($token, SECRET_KEY, array(ALGORITHM));
-            $realTime = $jwt->exp - time();
-
-            if($realTime < 1800){
-                $newData = json_decode(json_encode($jwt->data), true);
-                $newJwt = self::newToken($newData, 7200);
-                $token = $newJwt;
+        $dbObj = DB::getInstance();
+        $query = $dbObj->getQuery('SELECT * FROM users_sessions WHERE jwt =:jwt AND activate = 1');
+        $query->execute([
+            'jwt' => $token,
+        ]);
+        $data = $query->fetchAll(\PDO::FETCH_ASSOC);
+        if($data){
+            if(!$token){
+                $message['response'] = 'Token empty!.';
+                return $message;
             }
-            $message['result'] = true;
-            $message['jwt'] = $jwt->data;
-            $message['token'] = $token;
-           return $message;
-        }catch (\FireBase\JWT\ExpiredException $e) {
+
+            try{
+                $jwt = \Firebase\JWT\JWT::decode($token, SECRET_KEY, array(ALGORITHM));
+                $realTime = $jwt->exp - time();
+
+                if($realTime < 1800){
+                    $newData = json_decode(json_encode($jwt->data), true);
+                    $newJwt = self::newToken($newData, 7200);
+                    $token = $newJwt;
+                }
+                $message['result'] = true;
+                $message['jwt'] = $jwt->data;
+                $message['token'] = $token;
+                return $message;
+            }catch (\FireBase\JWT\ExpiredException $e) {
+                self::deleteToken();
+                $message['response'] = 'Your session has expired! Re login.';
+                return $message;
+            }catch (\FireBase\JWT\SignatureInvalidException $e){
+                $message['response'] =  'Verification failed.';
+                return $message;
+            }catch (\UnexpectedValueException $e){
+                $message['response'] =  'You must be logged in.';
+                return $message;
+            }
+        }else{
             self::deleteToken();
-            $message['response'] = 'Your session has expired! Re login';
-            return $message;
-        }catch (\FireBase\JWT\SignatureInvalidException $e){
-            $message['response'] =  'Verification failed';
-            return $message;
-        }catch (\UnexpectedValueException $e){
-            $message['response'] =  'You must be logged in.';
+            $message['response'] = 'You must log in again.';
             return $message;
         }
     }
