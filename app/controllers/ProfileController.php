23f7204db342d154015da8e2651f34587fbde61a
Marcos Gin <marcosgin291@gmail.com>
2017-08-04 21:04:44 -0300

    The settings path now receives a parameter of the type of action, we verify it to the parameter. We solve an error when we get the jwt of the header. We use the new method to update the password of the user.

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -17,44 +17,62 @@ class ProfileController extends BaseController{
     public function getIndex(){
         return $this->render('profile/index.twig', []);
     }
-    public function putSettings(){
+    public function putSettings($params = null){
         parse_str(file_get_contents("php://input"),$post_vars);//data amazing
 
-        $jwt = [];
+        $xhr = '';
         foreach (getallheaders() as $key => $value){
             if($key == 'Authorization'){
-                $jwt = Token::checkToken($value);
+                $xhr = $value;
             }
         }
+        $jwt = Token::checkToken($xhr);
         if($jwt['result']){
-            $upUser = $this->user->updateProfile($jwt['jwt']->id, $post_vars);
-            if($upUser['result']){
-                $user = $this->user->getUserForId($jwt['jwt']->id);
-                $this->user->downSessions($user[0]->id);
-                $newJwt = Token::newToken(['id' => $user[0]->id,
-                                            'device' => $_SERVER['HTTP_USER_AGENT'],
-                                            'ip' => $_SERVER['REMOTE_ADDR'],
-                                            'username' => $user[0]->name.' '.$user[0]->lastname,
-                                            'email' => $user[0]->email,
-                                            'admin' => $user[0]->rank], 7200);
-                $saveSession = $this->user->saveSession($user[0]->id, $newJwt);
-                if($saveSession){
-                    echo $this->json_response($upUser['response'], 200, $newJwt, true);
-                }
-            }else{
-                echo $this->json_response($upUser['response'], 200);
-            }
+                $option = true;
+                $upUser = [];
+              switch ($params){
+                  case 'information':
+                      $upUser = $this->user->updateProfile(12, $post_vars);
+                      break;
+                  case 'password':
+                      $upUser = $this->user->updatePassword(23, $post_vars);
+                      break;
+                  default:
+                      $option = false;
+              }
+              if($option){
+                  if($upUser['result']){
+                      $user = $this->user->getUserForId($jwt['jwt']->id);
+                      $this->user->downSessions($user[0]->id);
+                      $newJwt = Token::newToken(['id' => $user[0]->id,
+                          'device' => $_SERVER['HTTP_USER_AGENT'],
+                          'ip' => $_SERVER['REMOTE_ADDR'],
+                          'username' => $user[0]->name.' '.$user[0]->lastname,
+                          'email' => $user[0]->email,
+                          'admin' => $user[0]->rank], 7200);
+                      $saveSession = $this->user->saveSession($user[0]->id, $newJwt);
+                      if($saveSession){
+                          echo $this->json_response($upUser['response'], 200, $newJwt, true);
+                      }
+                  }else{
+                      echo $this->json_response($upUser['response'], 200);
+                  }
+              }else{
+                  echo $this->json_response('The path is incorrect', 400);
+              }
+
         }else{
             echo $this->json_response($jwt['response'], 200);
         }
     }
     public function getUser(){
-        $jwt = [];
+        $xhr = '';
         foreach (getallheaders() as $key => $value){
             if($key == 'Authorization'){
-                $jwt = Token::checkToken($value);
+                $xhr = $value;
             }
         }
+        $jwt = Token::checkToken($xhr);
         if($jwt['result']){
             $dataUser = $this->user->getProfile($jwt['jwt']->id);
             if ($dataUser['result']) {
